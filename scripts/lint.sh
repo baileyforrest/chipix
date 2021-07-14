0916e6dd785454037f60ff5e56478d28751796f1
Bailey Forrest <baileycforrest@gmail.com>
2021-07-13 21:32:42 -0700

    lint libcxx

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 SCRIPT_DIR=$(dirname "$0")
 cd $SCRIPT_DIR/..
 
@@ -8,4 +10,8 @@ FILES=$(find $DIRS -name "*.h" -o -name "*.cc")
 
 ./third_party/styleguide/cpplint/cpplint.py --verbose=0 \
     --filter=-legal/copyright,-build/header_guard,-build/c++11,-build/include_order,-whitespace/newline,-runtime/references \
-      --root=$PWD $FILES
+    --root=$PWD $FILES
+
+./third_party/styleguide/cpplint/cpplint.py --verbose=0 \
+    --filter=-legal/copyright,-build/header_guard,-build/c++11,-build/include_order,-whitespace/newline,-runtime/references,-build/include_what_you_use \
+    --root=$PWD $(find libcxx -name "*.h" -o -name "*.cc")
